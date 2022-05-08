599859d87f538e74f61979444fd96c520a6e665e
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-05-08 20:03:11 +0300

    edit code method (ResizeArray)

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] AS; 
+int count; 
+string[] AS2; 
+int len = 0;
+int index = 0;
+
+Console.Write("count = ");
+count = Int32.Parse(Console.ReadLine());
+
+AS = new string[count];
+
+Console.WriteLine("Fill in the array:");
+FillArray(AS);
+Console.WriteLine("\nArray:");
+PrintArray(AS);
+AS2 = new string[ResizeArray(AS, len)];
+NewArray(AS, AS2, index);
+Console.WriteLine("\nNew array:");
+if (AS2.Length == 0) Console.WriteLine("Таких строк нет.");
+else PrintArray(AS2);
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"array[{i}] = ");
+        array[i] = Console.ReadLine();
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");
+    }
+    Console.WriteLine();
+}
+
+int ResizeArray(string[] array, int size)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) size++;
+    }
+    return size;
+}
+
+void NewArray(string[] array, string[] new_array, int j)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            new_array[j] = array[i];
+            j++;
+        }
+    }
+}
